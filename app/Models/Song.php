1c489795295ff78e77788616c0bb90bdc66c90bf
Anh Tú Tạ <anhtu51195@gmail.com>
2021-06-08 00:43:17 +0700

    migrate create song model and define song schema

---
+++ app/Models/Song.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    // we'll allow the web app to fill data to any column on the table
+    protected $guarded = [];
+
+}
---
+++ database/migrations/2021_06_07_173438_create_song_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('song', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artist');
+            $table->string('album');
+            $table->string('type');
+            $table->integer('listens');
+            $table->string('file_name');
+            $table->string('image_name');
+            $table->string('image_url');
+            $table->dateTime('created_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('song');
+    }
+}
