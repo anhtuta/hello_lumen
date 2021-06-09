28ba1b3e80937e1e945c345a1a88c8d683c89d26
Anh Tú Tạ <anhtu51195@gmail.com>
2021-06-09 14:56:01 +0700

    Update cors: fix error in set response header

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -29,7 +29,7 @@ class CorsMiddleware
 
         $response = $next($request);
         foreach ($headers as $key => $value) {
-            $response->header($key, $value);
+            $response->headers->set($key, $value);
         }
 
         return $response;
